070ee16f721573d6e6e53289e155d0ece3679071
Imam Slamet <imamslamet810@gmail.com>
2019-11-12 12:16:28 +0700

    revisi 1

--- js/main.js
+++ js/main.js
@@ -13,50 +13,6 @@ $(document).ready(function() {
     });
 
 
-    //Hamburger Icon
-    $('#nav-icon1').click(function() {
-        $(this).toggleClass('open');
-        $(".navigation").toggleClass("show-menu");
-    });
-
-    // Tab list
-
-    // fix header
-    $(window).scroll(function() {
-        if ($(window).scrollTop() >= 110) {
-            $("header").addClass("fixed-position");
-        } else {
-            $('header').removeClass('fixed-position');
-        }
-    });
-
-    // aos animate
-    AOS.init({
-        useClassNames: true,
-        initClassName: true,
-        animatedClassName: 'animated',
-
-    });
-
-    // modal form
-
-    $(".right__click").click(function() {
-        $(".frameModal").fadeIn(300);
-    });
-
-    $(".close-iframe").click(function() {
-        $(".frameModal").fadeOut(300);
-    });
-
-    // modal view
-    $(".right__click1").click(function() {
-        $(".frameModal1").fadeIn(300);
-    });
-
-    $(".modal1__close").click(function() {
-        $(".frameModal1").fadeOut(300);
-    });
-
 });
 
 
